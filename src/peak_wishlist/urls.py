fac4bb948b575ff9fc3a59a0d7f343828381e994
Ivan Marcano <ijmm15@gmail.com>
2026-04-14 00:51:42 -0500

    ajuste en mostrar listado de montañas por paises

--- src/peak_wishlist/urls.py
+++ src/peak_wishlist/urls.py
@@ -6,5 +6,6 @@ app_name = 'peak_wishlist'
 urlpatterns = [
     path('', views.index, name='index'),
     path('pais/', views.paises, name='pais'),
-    path('montana/', views.montanas ,  name='montanas')
+    path('montana/', views.montanas ,  name='montanas'),
+    path('pais/montanas/<int:pais_id>', views.montanas, name='montanas_por_pais'),
     ]
--- src/peak_wishlist/views.py
+++ src/peak_wishlist/views.py
@@ -8,6 +8,12 @@ def paises(request):
     query = models.Pais.objects.all()
     return render(request, "peak_wishlist/pais.html", {'paises': query})
 
-def montanas(request):
-    query = models.Montana.objects.all()
-    return render(request, "peak_wishlist/montana.html", {'montanas': query})
+def montanas(request, pais_id=None):
+    if pais_id:
+        pais =  models.Pais.objects.get(id =pais_id)
+        query = models.Montana.objects.filter(pais=pais)
+        extension_titulo = f" en {pais.nombre}"
+    else:
+        query = models.Montana.objects.all()
+        extension_titulo = " "
+    return render(request, "peak_wishlist/montana.html", {'montanas': query, "extension_titulo": extension_titulo})
